75b22b3f2e88bd80cd25826c494950329fc01632
Anatoly Popov <ana23123@yandex.ru>
2022-04-04 20:09:51 +0500

    fix: исправить ошибки

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,33 @@
 let editButton = document.querySelector('.profile__edit-button');
 let modalWindow = document.querySelector('.popup');
 let modalCloseBtn = document.querySelector('.popup__close');
-
-editButton.addEventListener('click', function () {
-  modalWindow.classList.add('popup_active');
-  nameInput.value = profileTitle.textContent;
-  jobInput.value = profileSubitle.textContent;
-});
-
-modalCloseBtn.addEventListener('click', function () {
-  modalWindow.classList.remove('popup_active');
-});
-
 let formElement = document.querySelector('.popup__container');
 let nameInput = document.querySelector('[name="last-name"]');
 let jobInput = document.querySelector('[name="info"]');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubitle = document.querySelector('.profile__subtitle');
 
+function openPopup() {
+  modalWindow.classList.add('popup_active');
+}
+
+function closePopup() {
+  modalWindow.classList.remove('popup_active');
+}
+
+editButton.addEventListener('click', function () {
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubitle.textContent;
+  openPopup();
+});
+
+modalCloseBtn.addEventListener('click', closePopup);
+
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileTitle.textContent = nameInput.value;
   profileSubitle.textContent = jobInput.value;
-  modalWindow.classList.remove('popup_active');
+  closePopup();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
