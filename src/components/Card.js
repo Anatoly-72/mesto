f2128f9fb15bb0d4b7dd70e81a21a57b6f17a0f4
Anatoly Popov <ana23123@yandex.ru>
2022-05-29 18:27:14 +0500

    feat: добавить функцию открытия картинки

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,7 @@
-// import { imagePopup, imageBig, imageCaption } from '../utils/constants.js';
-
 export class Card {
-  constructor(title, image, cardSelector, handleCardClick) {
-    this._title = title;
-    this._image = image;
+  constructor(name, link, cardSelector, handleCardClick) {
+    this._name = name;
+    this._link = link;
     this._cardSelector = cardSelector;
     this._handleCardClick = handleCardClick;
   }
@@ -27,15 +25,9 @@ export class Card {
   }
 
   _setEventListeners() {
-    this._imageSmall.addEventListener('click', () => {
+    this._imageCard.addEventListener('click', () => {
       this._handleCardClick(this._name, this._link);
     });
-
-    // this._element
-    //   .querySelector('.card__img')
-    //   .addEventListener('click', () =>
-    //     this._handleCardClick(this._name, this._link)
-    //   );
 
     this._element
       .querySelector('.card__trash')
@@ -50,14 +42,12 @@ export class Card {
 
   generateCard() {
     this._element = this._getTemplate();
-    this._element.querySelector('.card__title').textContent = this._title;
+    this._element.querySelector('.card__title').textContent = this._name;
     this._likeBtn = this._element.querySelector('.card__icon');
-    this._imageSmall = this._element.querySelector('.card__img');
-    // const imageSmall = this._element.querySelector('.card__img');
+    this._imageCard = this._element.querySelector('.card__img');
+    this._imageCard.src = this._link;
+    this._imageCard.alt = this._name;
     this._setEventListeners();
-    this._imageSmall.src = this._image;
-    this._imageSmall.alt = this._title;
-    // this._setEventListeners();
 
     return this._element;
   }
