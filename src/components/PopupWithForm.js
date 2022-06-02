d96cc22d54d464f48a8bc178f144566c334815d4
Anatoly Popov <ana23123@yandex.ru>
2022-06-02 21:14:12 +0500

    fix: исправить ошибки

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -5,12 +5,12 @@ export default class PopupWithForm extends Popup {
     super(popupSelector);
     this._handleFormSubmit = handleFormSubmit;
     this._form = this._popup.querySelector('.popup__form');
-  }
-
-  _getInputValues() {
     this._inputList = Array.from(
       this._form.querySelectorAll('.popup__form-input')
     );
+  }
+
+  _getInputValues() {
     this._formValues = {};
     this._inputList.forEach(
       (input) => (this._formValues[input.name] = input.value)
